b8fc993af6bca36ce024adb2a0f24ab84e99fd45
Dagmawi Ephrem <dagmawiephrem44@gmail.com>
2026-08-07 10:08:53 +0300

    fix: typo error

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -194,7 +194,7 @@ export const refreshAccessToken = async (
         user.refreshToken = newRefreshToken;
         await user.save();
 
-        res.cookie('refreshToken', newRefreshToken, getRefreshCookieOptions());
+        res.cookie('refresh', newRefreshToken, getRefreshCookieOptions());
 
         res.status(200).json({
             accessToken: newAccessToken,
